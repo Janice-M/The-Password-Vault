d5efe79b470b8594381da1a7b4fe18a28e3d920f
Janice-M <janiceink001@gmail.com>
2019-07-19 13:07:38 +0300

    Added input username and password

--- password.py
+++ password.py
@@ -1,2 +1,15 @@
 class User:
-    pass
+    """ greeting = "Hello"
+    school = "Moringa"
+
+    print(greeting + ", " + school + "!") """
+    
+    print("Rafiki enter username:")
+    name = input()
+    
+    print("Rafiki enter password:")
+    password = int(input ())
+    print("username: "+ name+ ""+ "password: " +  str(password))
+    
+    
+pass
