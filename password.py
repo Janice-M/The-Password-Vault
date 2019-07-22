711011e938c3f681a912cfa13cdfd99df894125a
Janice-M <janiceink001@gmail.com>
2019-07-22 05:39:57 +0300

    added while lopp that is working fluidly

--- password.py
+++ password.py
@@ -3,65 +3,69 @@ import string
 import pyperclip
 
 global userList
+
+def passGen(size = 8, char=string.ascii_uppercase + string.ascii_lowercase + string.digits):
+    gen = ''.join(random.choice(char) for _ in range(size))
+    return gen
+    
 class User:
     def __init__(self,loginUsername,loginPassword):
         self.loginUsername =loginUsername
         self.loginPassword =loginPassword 
-    def  saveUser (self):
-        """ This function saves the user details """
-        User.userList.append(self)
     
-        
-    def passGen(size = 8, char=string.ascii_uppercase + string.ascii_lowercase + string.digits):
-        gen = ''.join(random.choice(char) for _ in range(size))
-        return gen
-    """ The above function helps generate passwords using .random() """
-    userList=[]
+    userList =[]
+    
     print('Rafiki, welcome to The Password Vault')
+    while  True:
+        print("Rafiki,select a number")
+        print("1. Login ")
+        print("2. Sign up ")
+        option_selected = int(input())
+        if option_selected == 1:
+            print( "Welcome consistent rafiki")
+            print("Kindly, enter your username:")
+            loginUsername= input()
+            print("Kindly enter your password")
+            loginPassword= input()
+            """ The above code provides a login method for users of the The Password Vault """
+        
+        elif option_selected == 2:
+            print ("Welcome new rafiki")
+            print(" Create your username:")
+            newUsername= input()
+            print ("Rafiki, select a number")
+            print("1. Create your own password")
+            print("2. Generate password ")
+            newPassword= int(input())
+        
+            if newPassword == 1: 
+                print("Enter password below")
+                newPassword = input()
+            
+                userList.append([newUsername,newPassword])
+                print(userList)
+            
+            elif newPassword == 2:
+                generator = passGen()
+                userList.append([newUsername,generator])
+                print(userList)
+                """ saveUser is to save the user details using the method above  """          
+                """ the elif option is used as it can take in arguements for users who would like to generate passwords """
+        
+                print("Your Password Vault has been created successfully rafiki, " +newUsername)
+                print ("Please do login now")
+                print( "Welcome consistent rafiki")
+                print("Kindly, enter your username:")
+                loginUsername= input()
+                print("Kindly enter your password")
+                loginPassword= input()
+        else:
+            break
     
-    print("Rafiki,select a number")
-    print("1. Login ")
-    print("2. Sign up ")
-    option_selected = int(input())
-    if option_selected == 1:
-        print( "Welcome consistent rafiki")
-        print("Kindly, enter your username:")
-        loginUsername= input()
-        print("Kindly enter your password")
-        loginPassword= input()
-        """ The above code provides a login method for users of the The Password Vault """
-        
-    else:
-        print ("Welcome new rafiki")
-        print(" Create your username:")
-        newUsername= input()
-        print ("Rafiki, select a number")
-        print("1. Create your own password")
-        print("2. Generate password ")
-        newPassword= int(input())
-        if newPassword == 1: 
-            print("Enter password below")
-            newPassword = input()
-            
-        saveUser(newUsername,newPassword)
-            
-        elif newPassword == 2:
-            generator = passGen()
-        saveUser(newUsername, generator)
-            """ saveUser is to save the user details using the method above  """
-            
-        
-        """ the elif option is used as it can take in arguements for users who would like to generate passwords """
-        
-        print("Your Password Vault has been created successfully rafiki, " +newUsername)
-        print ("Please do login now")
-        print( "Welcome consistent rafiki")
-        print("Kindly, enter your username:")
-        loginUsername= input()
-        print("Kindly enter your password")
-        loginPassword= input()
-        
+
 class Credential:
+    
+    """ The above function helps generate passwords using .random() """
 
     """ this class holds the different credentials the users need to be kept in the Password Vault """
 
