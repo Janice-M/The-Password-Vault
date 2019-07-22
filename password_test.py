d2f53abc4b01fdaffcf3f5a5d708804c90af4475
Janice-M <janiceink001@gmail.com>
2019-07-22 11:17:27 +0300

    wrote tests

--- password_test.py
+++ password_test.py
@@ -1,6 +1,6 @@
 import unittest
 import pyperclip
-#From password.py import the classes
+
 from password import User, Credential
 
 class TestUser (unittest.TestCase):
@@ -8,12 +8,19 @@ class TestUser (unittest.TestCase):
     Args: unittest.TestCase: helps in creating test cases '''
     def setUp(self):
         ''' Function to create a user account before each test '''
+        
         self.test_user = User('Janice','somepassword0123')
 
     def test__init__(self):
         ''' Test to if check the initialization/creation of user instances is properly done '''
+        
         self.assertEqual(self.test_user.loginUsername,'Janice')
         self.assertEqual(self.test_user.loginPassword,'somepassword0123')
+    
+    def verify(self):
+        """ This function verifies togin to enable users to access their vault """
+        self.test_user=  User('Janice','somepassword0123')
+        
 
 if __name__ == '__main__':
-	unittest.main()
+	unittest.main(verbosity=2)
